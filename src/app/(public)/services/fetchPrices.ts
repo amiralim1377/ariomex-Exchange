bd8e59a80fe2d2365856d3dc963037c5acb2cd15
AMIRALI <alimoradi7718@gmail.com>
2025-12-06 02:05:14 +0330

    change some folder structure

--- src/app/(public)/services/fetchPrices.ts
+++
@@ -1,9 +0,0 @@
-import Coin from "@/types/Coin";
-
-async function fetchPrices(): Promise<Coin[]> {
-  const res = await fetch("/api/prices");
-  if (!res.ok) throw new Error("Failed to fetch prices");
-  return res.json();
-}
-
-export default fetchPrices;
