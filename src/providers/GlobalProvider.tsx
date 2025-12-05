d6454dac4d12670bced7163a8fdb3486cbb9bddd
AMIRALI <alimoradi7718@gmail.com>
2025-12-06 02:13:59 +0330

    update global provider

--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -3,21 +3,18 @@
 import { ScrollProvider } from "@/context/ScrollContext";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { ReactNode } from "react";
-import { QueryClientProviderWrapper } from "./QueryClientProviderWrapper";
 
 export const GlobalProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProviderWrapper>
-      <ScrollProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </ScrollProvider>
-    </QueryClientProviderWrapper>
+    <ScrollProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </ScrollProvider>
   );
 };
