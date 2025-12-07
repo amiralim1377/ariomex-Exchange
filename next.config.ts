92e74732f413530b61425e90539a747f69a02238
AMIRALI <alimoradi7718@gmail.com>
2025-12-07 13:47:03 +0330

    update platformBenefits components-svg route

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: false,
+  reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
   output: "export",
 };
 
--- src/components/PlatformBenefits/PlatformBenefits.tsx
+++ src/components/PlatformBenefits/PlatformBenefits.tsx
@@ -7,22 +7,22 @@ export default function PlatformBenefits() {
         <FeatureCard
           title="بازارهای تومان و تتر"
           description="در بیش از ۲۰۰ بازار معاملاتی خرید و فروش کنید"
-          icon="/PlatformBenefits/eco1.svg"
+          icon="./PlatformBenefits/eco1.svg"
         />
         <FeatureCard
           title="سفارش حدضرر و حدسود"
           description="همزمان دو سفارش برای سود و زیان داشته باشید"
-          icon="/PlatformBenefits/eco2.svg"
+          icon="./PlatformBenefits/eco2.svg"
         />
         <FeatureCard
           title="احراز هویت سریع"
           description="در کمتر از چند دقیقه احراز هویت کرده و شروع به معامله کنید"
-          icon="/PlatformBenefits/eco3.svg"
+          icon="./PlatformBenefits/eco3.svg"
         />
         <FeatureCard
           title="رابط API پیشرفته"
           description="با API آریومکس وارد دنیای معاملات الگوریتمی شوید"
-          icon="/PlatformBenefits/eco4.svg"
+          icon="./PlatformBenefits/eco4.svg"
         />
       </section>
     </div>
